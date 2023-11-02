fd819eab196ba1f4fe2ad8d09cb30d5f0ce861a1
digit81 <26637307+digit81@users.noreply.github.com>
2023-11-02 18:53:15 +0100

    show two minimal flip Swiper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {register} from 'swiper/element/bundle';
+import {Swiper, SwiperOptions} from "swiper/types";
+register();
+
+interface HTMLSwiperElement extends HTMLElement {
+  swiper: Swiper,
+  initialize: () => void,
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
-  title = 'swiper-v9';
+export class AppComponent implements OnInit {
+
+  readonly swiperParams: SwiperOptions = {
+    effect: 'flip',
+    cubeEffect: {
+      shadow: false,
+      slideShadows: true,
+    },
+    flipEffect: {
+      limitRotation: true,
+      slideShadows: true,
+    },
+    grabCursor: true,
+    pagination: true,
+    navigation: true,
+    enabled: true,
+    on: {
+      beforeTransitionStart: () => window.getSelection()?.removeAllRanges(),
+      init() {
+        console.log('INITIALIZED');
+      },
+    },
+  };
+
+
+  ngOnInit() {
+    this.initMultipleSwiper()
+  }
+
+  private initMultipleSwiper(): void {
+    const swiperElements = document.querySelectorAll<HTMLSwiperElement>('swiper-container');
+    swiperElements.forEach(el => this.initSwiper(el));
+  }
+
+  private initSwiper(swiperEl: HTMLSwiperElement): void {
+    Object.assign(swiperEl, this.swiperParams);
+    swiperEl.initialize();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
@@ -11,6 +11,10 @@ import { AppComponent } from './app.component';
     BrowserModule
   ],
   providers: [],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
